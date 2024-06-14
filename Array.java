1b8160ecfd1da3cb9de2b90163005ea415eb6759
JPC-Code010 <sanandsin58@gmail.com>
2024-06-14 14:42:10 +0000

    Array

--- Array.java
+++ Array.java
@@ -1,1 +1,29 @@
 
+public class Array{
+    public static void main (String[] arg){
+      
+        ///Array walking 
+        //Forward
+        int [] a = {2,4,6,8,10};
+
+        for (int i= 0; i< a.length;i++){
+             System.out.print(a[i] + " ");
+        }
+        
+        System.out.println(" ");
+
+        //Forward, using enhanced for
+        for (int value:a){
+            System.out.print(value + " ");
+        }
+
+        System.out.println(" ");
+        
+        //Backward
+        for(int i = a.length-1; i>= 0 ;i--){
+            System.out.print(a[i] + " ");
+
+        }
+
+    }
+}
